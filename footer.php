e295fd06ff6688262152ff642f7ca7bb8841f431
Forrest Wilson <forrest.wilson@me.com>
2018-05-04 11:45:15 +1200

    Footer styling

--- footer.php
+++ footer.php
@@ -11,26 +11,13 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'udemy-bootstrap' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'udemy-bootstrap' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'udemy-bootstrap' ), 'udemy-bootstrap', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
+
+<!-- JQUERY -->
+<script src="<?php bloginfo("template_directory"); ?>/node_modules/jquery/dist/jquery.min.js"></script>
+<script src="<?php bloginfo("template_directory"); ?>/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<script src="<?php bloginfo("template_directory"); ?>/assets/js/main.js"></script>
 
 </body>
 </html>
